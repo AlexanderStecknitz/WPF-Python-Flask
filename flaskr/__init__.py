1956e847aea860bd5e355ed0097ea9c0729c92f4
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-08 20:52:58 +0200

    feat: added logging config

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 from rest.kunde_get_controller import kunde_getcontroller
 from container import Container
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] [%(levelname)s] %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
 def create_app() -> Flask:
