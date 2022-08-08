56157613492c0e7571e4220072ee5dd7504909bf
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-08 20:39:32 +0200

    feat: added mock objects

---
+++ flaskr/repository/kunden.py
@@ -0,0 +1,54 @@
+from entity.kunde import Kunde
+
+kunden = [
+    Kunde(
+        id=1,
+        name="Mock1",
+        vorname="Mock1"
+    ),
+    Kunde(
+        id=2,
+        name="Mock2",
+        vorname="Mock2"
+    ),
+    Kunde(
+        id=3,
+        name="Mock3",
+        vorname="Mock3"
+    ),
+    Kunde(
+        id=4,
+        name="Mock4",
+        vorname="Mock4"
+    ),
+    Kunde(
+        id=5,
+        name="Mock5",
+        vorname="Mock5"
+    ),
+    Kunde(
+        id=6,
+        name="Mock6",
+        vorname="Mock6"
+    ),
+    Kunde(
+        id=7,
+        name="Mock7",
+        vorname="Mock7"
+    ),
+    Kunde(
+        id=8,
+        name="Mock8",
+        vorname="Mock8"
+    ),
+    Kunde(
+        id=9,
+        name="Mock9",
+        vorname="Mock9"
+    ),
+    Kunde(
+        id=10,
+        name="Mock10",
+        vorname="Mock10"
+    ),
+]
