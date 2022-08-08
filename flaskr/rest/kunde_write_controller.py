1db5ddc3e0ea43ac35cd00cee03f13e4fdde5afd
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-09 00:03:33 +0200

    fix: added @inject decorator

--- flaskr/rest/kunde_write_controller.py
+++ flaskr/rest/kunde_write_controller.py
@@ -5,10 +5,11 @@ from services.kunde_write_service import KundeWriteService
 
 kunde_write_controller = Blueprint("KundeWriteController", __name__)
 
-@kunde_write_controller.post('/api/<int:kunde_id>')
+
+@kunde_write_controller.delete('/api/<int:kunde_id>')
+@inject
 def delete_by_id(kunde_id,
                  write_service: KundeWriteService = Provide[Container.kunde_write_service]):
     current_app.logger.info('delete kunde with id: %s', kunde_id)
-    write_service.delete(kunde_id = kunde_id)
-    return 201
-
+    write_service.delete_by_id(kunde_id=kunde_id)
+    return jsonify(201)
