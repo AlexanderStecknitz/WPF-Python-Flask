6a7e9a8128d757006259fa6b415a81544d47e788
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-08 21:53:04 +0200

    feat: find with query params in GetController

--- flaskr/rest/kunde_get_controller.py
+++ flaskr/rest/kunde_get_controller.py
@@ -1,22 +1,19 @@
 """Test"""
 
-from flask import Blueprint, jsonify, current_app
+from flask import Blueprint, jsonify, current_app, request
 from dependency_injector.wiring import inject, Provide
 from services.kunde_read_service import KundeReadService
 from container import Container
 
 kunde_getcontroller = Blueprint('KundeGetController', __name__)
 
-
 @kunde_getcontroller.get('/api')
 @inject
-def find_all(
-        read_service: KundeReadService = Provide[Container.kunde_read_service]
+def find(
+    read_service: KundeReadService = Provide[Container.kunde_read_service]
 ):
-    """
-    Test
-    """
-    current_app.logger.info('find_all kunden')
-    kunde = read_service.find_all()
-    current_app.logger.info('find all kunden: %s', kunde)
-    return jsonify(kunde)
+    args = request.args
+    current_app.logger.info('find %s', args)
+    kunden = read_service.find(args)
+    return jsonify(kunden)
+
