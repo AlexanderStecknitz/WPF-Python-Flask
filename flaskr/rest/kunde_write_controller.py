d397e50442eb90108f72a7f9d5458d3a6a374147
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-09 11:11:32 +0200

    feat: added WriteController Error Handling

--- flaskr/rest/kunde_write_controller.py
+++ flaskr/rest/kunde_write_controller.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify, current_app, request
+from flask import Blueprint, jsonify, current_app, json
 from dependency_injector.wiring import inject, Provide
 from container import Container
 from services.kunde_write_service import KundeWriteService
+from werkzeug.exceptions import HTTPException
 
 kunde_write_controller = Blueprint("KundeWriteController", __name__)
 
@@ -13,3 +14,14 @@ def delete_by_id(kunde_id,
     current_app.logger.info('delete kunde with id: %s', kunde_id)
     write_service.delete_by_id(kunde_id=kunde_id)
     return jsonify(201)
+
+@kunde_write_controller.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
