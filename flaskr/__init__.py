898339f0741cc34176599427f694075d2db1aac0
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-08-09 11:09:52 +0200

    fix: refactor blueprint name of GetController

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from rest.kunde_get_controller import kunde_getcontroller
+from rest.kunde_get_controller import kunde_get_controller
 from rest.kunde_write_controller import kunde_write_controller
 from container import Container
 from logging.config import dictConfig
@@ -26,7 +26,7 @@ def create_app() -> Flask:
     container = Container()
     app = Flask(__name__)
     app.container = container
-    app.register_blueprint(kunde_getcontroller)
+    app.register_blueprint(kunde_get_controller)
     app.register_blueprint(kunde_write_controller)
 
     @app.route('/')
